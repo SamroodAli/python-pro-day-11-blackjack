d6b32aa3a8689c1e7f38939639bbef5e438377f3
Samrood Ali <samroodali@live.com>
2020-12-08 18:17:24 +0000

    blackjack rules removed from code

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ import random
 
 ############### Blackjack Project #####################
 
-############### Our Blackjack House Rules #####################
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
 cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 
 def deal_new_card(list,is_player=False):
