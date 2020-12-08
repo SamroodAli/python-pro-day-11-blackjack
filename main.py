5dfb69f2def1dfbc12b1af9aa0f1cf4bb34dbe8b
Samrood Ali <samroodali@live.com>
2020-12-08 17:57:29 +0000

    print message format fixes

--- main.py
+++ main.py
@@ -19,7 +19,14 @@ cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 def deal_new_card(list,is_player=False):
     card = random.choice(cards)
     if is_player:
-        print(f"Drew {card}")
+        list_length=len(list)
+        if list_length==1:
+            print(f" and {card}")
+        else:
+            if list_length==0:
+                print(f"You drew {card}",end="")
+            else:
+                print(f"You drew {card}")
     if card == 'A':
         if 11 in list:
             list.append(1)
@@ -48,7 +55,8 @@ def blackjack():
     def check_winner():
         sum_pc = sum(pc_hand)
         sum_player=sum(player_hand)
-        print(f"your hand = {player_hand}\ntotal = {sum_player}\npc hand = {pc_hand}\ntotal = {sum_pc}")
+        print(f"your hand = {player_hand} and total = {sum_player}\npc hand = {pc_hand} and total = {sum_pc}\n")
+
         if sum_player >21:
             print("You bust")
         elif sum_pc >21:
@@ -67,7 +75,7 @@ def blackjack():
         deal_new_card(pc_hand)
         sum_pc = sum(pc_hand)
         sum_player=sum(player_hand)
-    print(f"Your hand : {player_hand}\ntotal = {sum_player}\nPc hand : {pc_hand[0]}")
+    print(f"Your hand : {player_hand} and total = {sum_player}\nPc hand : {pc_hand[0]}")
     game_over = False
     while sum_player < 22 and not game_over:
         while sum_pc < 17:
